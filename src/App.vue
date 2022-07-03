bd7b4a152fa5846d37a2576bc2c2971a832800aa
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-07-03 10:15:17 +0300

    Add Animation on page background

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-    </v-app-bar>
-
+    <AppBar/>
     <v-main>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import AppBar from './components/AppBar.vue';
 export default {
   name: 'App',
 
   components: {
-  },
+    AppBar
+},
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+.v-main {
+	background: linear-gradient(-45deg, #0a0a22,  #380f57, #0b0524, #130639);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+	height: 100vh;
+}
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+
+</style>
